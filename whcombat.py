8e505fd2b91aa04a2f5b60ff2df1868041cfd575
ohwaitwhat <55898267+ohwaitwhat@users.noreply.github.com>
2023-10-14 13:34:16 -0500

    Add files via upload
    Added multi entity load from config file via drop down.  Combat class needs modification.

--- whcombat.py
+++ whcombat.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 import configparser
 import random
-#newlinecomment
+
 
 # ... [previous functions, including roll_d100, get_hit_location, critical_hit, etc.]
 def roll_d10():
@@ -80,63 +81,91 @@ class Character:
 
 
 class CombatApp:
-   def __init__(self, root):
-       self.root = root
-       self.root.title("WFRP Combat Simulator")
+    def __init__(self, root):
+        self.root = root
+        self.root.title("WFRP Combat Simulator")
+
+        # Load config
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+        
+        # Filter sections by player and enemy prefix
+        self.players = [section for section in self.config.sections() if "Player" in section]
+        self.enemies = [section for section in self.config.sections() if "Enemy" in section]
+
+        # Player Dropdown
+        self.selected_player = tk.StringVar()
+        self.player_dropdown = ttk.Combobox(root, textvariable=self.selected_player)
+        self.player_dropdown['values'] = self.players
+        self.player_dropdown.grid(row=0, column=1)
+        self.player_dropdown.bind("<<ComboboxSelected>>", self.load_player_data)
+
+        # Enemy Dropdown
+        self.selected_enemy = tk.StringVar()
+        self.enemy_dropdown = ttk.Combobox(root, textvariable=self.selected_enemy)
+        self.enemy_dropdown['values'] = self.enemies
+        self.enemy_dropdown.grid(row=0, column=3)
+        self.enemy_dropdown.bind("<<ComboboxSelected>>", self.load_enemy_data)
       
-       # Load character data from config
-       config = configparser.ConfigParser()
-       config.read('config.ini')
+        # Player Labels and Data
+        self.player_label = tk.Label(root, text="Player")
+        self.player_label.grid(row=0, column=0, padx=10, pady=10)
 
 
-       player_attributes = config['Player']
-       enemy_attributes = config['Enemy']
+        self.player_wounds = tk.Label(root, text=f"Wounds: {self.player.wounds}")
+        self.player_wounds.grid(row=1, column=0, padx=10, pady=10)
 
 
-       self.player = Character("Player", int(player_attributes['weapon_skill']), int(player_attributes['wounds']), int(player_attributes['strength']))
-       self.enemy = Character("Enemy", int(enemy_attributes['weapon_skill']), int(enemy_attributes['wounds']), int(enemy_attributes['strength']))
-      
-       # Player Labels and Data
-       self.player_label = tk.Label(root, text="Player")
-       self.player_label.grid(row=0, column=0, padx=10, pady=10)
+        # Enemy Labels and Data
+        self.enemy_label = tk.Label(root, text="Enemy")
+        self.enemy_label.grid(row=0, column=1, padx=10, pady=10)
 
 
-       self.player_wounds = tk.Label(root, text=f"Wounds: {self.player.wounds}")
-       self.player_wounds.grid(row=1, column=0, padx=10, pady=10)
+        self.enemy_wounds = tk.Label(root, text=f"Wounds: {self.enemy.wounds}")
+        self.enemy_wounds.grid(row=1, column=1, padx=10, pady=10)
 
 
-       # Enemy Labels and Data
-       self.enemy_label = tk.Label(root, text="Enemy")
-       self.enemy_label.grid(row=0, column=1, padx=10, pady=10)
+        # Action Button
+        self.attack_button = tk.Button(root, text="Attack!", command=self.attack)
+        self.attack_button.grid(row=2, column=0, columnspan=2, pady=20)
 
 
-       self.enemy_wounds = tk.Label(root, text=f"Wounds: {self.enemy.wounds}")
-       self.enemy_wounds.grid(row=1, column=1, padx=10, pady=10)
+        # Log Textbox
+        self.log_text = tk.Text(root, wrap=tk.WORD, height=10, width=50)
+        self.log_text.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+    def load_player_data(self, event):
+        player_data = self.config[self.selected_player.get()]
+        self.player_ws_entry.delete(0, tk.END)
+        self.player_ws_entry.insert(0, player_data['weapon_skill'])
+        self.player_str_entry.delete(0, tk.END)
+        self.player_str_entry.insert(0, player_data['strength'])
+        self.player_wounds_entry.delete(0, tk.END)
+        self.player_wounds_entry.insert(0, player_data['wounds'])
+
+    def load_enemy_data(self, event):
+        enemy_data = self.config[self.selected_enemy.get()]
+        self.enemy_ws_entry.delete(0, tk.END)
+        self.enemy_ws_entry.insert(0, enemy_data['weapon_skill'])
+        self.enemy_str_entry.delete(0, tk.END)
+        self.enemy_str_entry.insert(0, enemy_data['strength'])
+        self.enemy_wounds_entry.delete(0, tk.END)
+        self.enemy_wounds_entry.insert(0, enemy_data['wounds'])
 
 
-       # Action Button
-       self.attack_button = tk.Button(root, text="Attack!", command=self.attack)
-       self.attack_button.grid(row=2, column=0, columnspan=2, pady=20)
-
-
-       # Log Textbox
-       self.log_text = tk.Text(root, wrap=tk.WORD, height=10, width=50)
-       self.log_text.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
-
-
-   def log(self, message):
+    def log(self, message):
        """Add a message to the log textbox."""
        self.log_text.insert(tk.END, message + "\n")
        self.log_text.see(tk.END)
 
 
-   def update_wounds(self):
+    def update_wounds(self):
        """Update the displayed wounds for both characters."""
        self.player_wounds.config(text=f"Wounds: {self.player.wounds}")
        self.enemy_wounds.config(text=f"Wounds: {self.enemy.wounds}")
 
 
-   def attack(self):
+    def attack(self):
        """Simulate a round of combat."""
        # Player attacks enemy
        roll = roll_d100()
@@ -187,7 +216,7 @@ class CombatApp:
            self.log("Enemy misses!")
 
 
-   def end_combat(self, message):
+    def end_combat(self, message):
        """End the combat and display a message."""
        self.attack_button.config(state=tk.DISABLED)
        messagebox.showinfo("Combat Ended", message)
